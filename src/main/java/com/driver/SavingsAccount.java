dbbdf789ba752b2be02cd9fdb5121d50babf792a
Sourav <sourav10singh11@gmail.com>
2023-05-26 11:42:14 +0530

    Changed a method

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -34,7 +34,7 @@ public class SavingsAccount extends BankAccount{
     public double getSimpleInterest(int years){
         // Return the final amount considering that bank gives simple interest on current amount
 
-        double si = super.getBalance()*rate*years;
+        double si = super.getBalance() * (1+(rate * years)/100);
         return si;
     }
 
